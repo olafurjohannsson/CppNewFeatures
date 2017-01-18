ff4ef9360f44a36c72c8f851f013e26f37c3884a
olafurj <olafurj@olafurj.tern.is>
2017-01-18 16:16:41 +0000

    traits

--- intermediate/type_traits.cpp
+++ intermediate/type_traits.cpp
@@ -3,33 +3,53 @@
 #include <iostream>
 #include <type_traits>
 #include <memory>
+#include <cxxabi.h>
 // is T is a fundamental type (arithmetic type, void or nullptr_t)
 
-template<class T>
+    template<class T>
 typename std::enable_if<std::is_fundamental<T>::value, std::string>::type stringify(const T& t)
 {
     std::cout << "1\n";
     return std::to_string(t);
 }
 
-template<class T>
+    template<class T>
 typename std::enable_if<!std::is_fundamental<T>::value, std::string>::type stringify(const T& t)
 {
     std::cout << "2\n";
     return std::to_string(t);
 }
 
-template <typename T, typename A1>
-std::unique_ptr<T> factory(A1&& a1)
+template<typename T>
+struct is_ptr { static const bool value = false; };
+template<typename T>
+struct is_ptr<T*> { static const bool value = true; };
+
+template <typename T> std::string type_name()
 {
-    return std::unique_ptr<T>(std::forward<T>(new T(a1)));
+    typedef typename std::remove_reference<T>::type TR;
+
+    std::unique_ptr<char, void(*)(void*)> own
+        (
+
+        );
 }
-void test() {}
+namespace t
+{
+    int test()
+    {
+        std::cout << __func__ << std::endl;
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        std::cout << __FUNCTION__ << std::endl;
+        std::cout << stringify(3.0f) << std::endl;
+        std::cout << stringify(false) << std::endl;
+        int i;
+        typedef decltype(i) i_type;
+    }
+}
+
 int main()
 {
-    std::cout << stringify(3.0f) << std::endl;
-    std::cout << stringify(false) << std::endl;
-
-
+    t::test();
 }
 
